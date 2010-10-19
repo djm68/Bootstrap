1d8e8a0fafdda5c8c2db35eaed1f6f4dab2287d7
Dominic Maraglia <dmaraglia@gmail.com>
2010-10-19 16:53:25 -0700

    Fixed error in ifcfg

--- modules/ifcfg_eth0/manifests/init.pp
+++ modules/ifcfg_eth0/manifests/init.pp
@@ -1,26 +1,35 @@
 # Setup ifcfg-eth0 with fixed IP
 class ifcfg_eth0 {
+
 case $hostname {
   pmaster: { 
     file{
       "/etc/sysconfig/network-scripts/ifcfg-eth0":
       ensure  => present,
-      source  => '/etc/puppet/modules/ifcfg_eth0/files',
+      source  => '/etc/puppet/modules/ifcfg_eth0/files/ifcfg-eth0.pmaster',
+    }
+  }
+  pmaster: { 
+    file{
+      "/etc/sysconfig/network-scripts/ifcfg-eth0":
+      ensure  => present,
+      source  => '/etc/puppet/modules/ifcfg_eth0/files/ifcfg-eth0.pagent',
     }
   }
   qa1: { 
     file{
       "/etc/sysconfig/network-scripts/ifcfg-eth0":
       ensure  => present,
-      source  => '/etc/puppet/modules/ifcfg_eth0/files',
+      source  => '/etc/puppet/modules/ifcfg_eth0/files/ifcfg-eth0.qa1',
     }
   }
   mqhq: { 
     file{
       "/etc/sysconfig/network-scripts/ifcfg-eth0":
       ensure  => present,
-      source  => '/etc/puppet/modules/ifcfg_eth0/files',
+      source  => '/etc/puppet/modules/ifcfg_eth0/files/ifcfg-eth0.mqhq',
     }
   }
 }
+
 }
